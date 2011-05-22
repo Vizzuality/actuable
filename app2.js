d8e0360a819df833e6b5a51dc81ccad840be0ec6
Vizzuality <contact@vizzuality.com>
2011-05-22 17:50:38 +0100

    test ip address

--- app2.js
+++ app2.js
@@ -1,28 +1,35 @@
 var fs = require('fs');
-var geoip = require('geoip');
-var nowjs = require("now");
 var express = require('express');
-var city = new geoip.City(__dirname + '/GeoLiteCity.dat'); 
+
 var app = express.createServer();
 app.configure(function(){
     app.use(express.methodOverride());
     app.use(express.bodyParser());
     app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
+    // app.use(express.static(__dirname + '/public'));
 });
-
 
 
 app.get('/', function(req, res){
   var ip_address = null;
   try {
-    ip_address = req.headers['x-forwarded-for'];
-  }
-  catch ( error ) {
     ip_address = req.connection.remoteAddress;
   }
-  res.contentType('html');  
-  res.send(ip_address)
+  catch ( error ) {
+    ip_address = "unknown";
+  }
+  res.contentType('html');
+  console.log(req.connection.remoteAddress);  
+  res.send("you asshole")
 });
-app.listen(8181);
 
+//app.listen(8080);
+
+
+// var app = require('express').createServer();
+// 
+// app.get('/', function(req, res){
+//   res.send('hello world');
+// });
+
+app.listen(8181);
